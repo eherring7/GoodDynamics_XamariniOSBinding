71d7065b18302385a258ea108ee04d68a181fbfc
Greg Gammon <ggammon@westmonroepartners.com>
2015-06-02 12:52:22 -0500

    Documentation.

--- GoodDynamicsBinding/StructsAndEnums.cs
+++ GoodDynamicsBinding/StructsAndEnums.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using Foundation;
 
 namespace GoodDynamics
 {
@@ -18,7 +19,6 @@ namespace GoodDynamics
 		public static extern int sqlite3enc_import (IntPtr srcFilename, IntPtr destFilename);
 	}
 
-
 	public enum GDAppResultCode{
 		ErrorNone = 0,
 		ErrorActivationFailed = -101,
@@ -34,32 +34,101 @@ namespace GoodDynamics
 		ErrorSecurityError = -100
 	}
 
+	/** \struct GDFileStat
+	 * Information about a file or directory in the secure store.
+	 * This structure is used to return information about a file or
+	 * directory in the secure store.
+	 * \see \link GDFileSystem.GetFileStat GetFileStat\endlink
+	 */
 	[StructLayout (LayoutKind.Sequential)]
 	public struct GDFileStat{
 		public int fileLen;
+		/**< File size in bytes */
+
 		public ulong lastModifiedTime;
+		/**< Timestamp for when file was last modified */
+
 		public bool isFolder;
+		/**< <TT>true</TT> for directories, <TT>false</TT> for plain files */
 	}
 
-	public enum GDFileSystemErr : uint {
-		GDFileSystemErrPathDoesntExist = 100,
-		GDFileSystemErrIOError = 101,
-		GDFileSystemErrPermissionError = 102,
-		GDFileSystemErrDirNotEmpty = 103,
-		GDFileSystemErrUnknownError = 500
-	}
 
+	/**
+	 * \defgroup gdappevent GDAppEvent property constants
+	 * Use these enumerated constants in the application code for the
+	 * Good Dynamics Runtime event-handler.
+	 *
+	 * \{
+	 */
+
+	/** Constants for GDAppEvent type.
+	* This enumeration represents the type of a GDAppEvent that is being
+	* notified. The \ref GDAppEvent.Type property will always take one of these
+	* values.
+	*/
 	public enum GDAppEventType{
 		Authorized = 0,
+		/**< Either the user has been authorized to access the application and its
+	     * data, following authorization processing, or a condition that caused
+	     * authorization to be withdrawn has been cleared.
+	     * In either case, the user can be given access to the application data, and
+	     * the application can make full use of the Good Dynamics API.
+	     * 
+	     * The event result code will be <TT>GDErrorNone</TT>.
+	     * 
+	     * See  \link GDiOS.Authorize Authorize (GDiOS)\endlink for authorization processing initiation.
+	     */
 		NotAuthorized = 1,
+		/**< Either the user has <EM>not </EM>been authorized to access the
+	     * application and its data, following authorization processing, or a
+	     * condition has arisen that caused authorization to be withdrawn.
+	     * In either case, the application must deny the user access to any
+	     * application data. This includes not displaying any data in the
+	     * application user interface.
+	     * 
+	     * In the case that the user is found not to be authorized following
+	     * authorization processing, the application cannot make use of the Good
+	     * Dynamics APIs, except to initiate authorization processing again.
+	     * Otherwise, if authorization has only been withdrawn, the application can
+	     * make use of the Good Dynamics APIs.
+	     * 
+	     * The event result code will indicate the condition that has arisen.
+	     * See \ref GDAppResultCode.
+	     * 
+	     * See  \link GDiOS.Authorize: Authorize (GDiOS)\endlink for authorization processing initiation.
+	     */
 		RemoteSettingsUpdate = 2,
+		/**< A change to application configuration or other settings from the
+	     * enterprise has been received.\ An event of this type is despatched
+	     * whenever there is a change in any value that is returned by
+	     * \link GDiOS.GetApplicationConfig GetApplicationConfig (GDiOS)\endlink.
+	     */
 		ServicesUpdate = 3,
-		PolicyUpdate = 4
+		/**< A change to services-related configuration of one or more applications
+	     * has been received.\ See under  \link GDiOS.GetServiceProvidersFor GetServiceProvidersFor:  (GDiOS)\endlink.
+	     */
+		PolicyUpdate = 4,
+		/**< A change to one or more application-specific policy settings has been
+	     * received.\ See under \link GDiOS.GetApplicationPolicy GetApplicationPolicy (GDiOS)\endlink.
+	     */
+		DataPlanUpdate = 5
+		/**< A change to the data plan state of the running application has been
+	     * received.\ See also \link GDiOS.IsUsingDataPlan IsUsingDataPlan (GDiOS)\endlink.
+	     */
 	}
+	/** \}
+ 	*/
 
+	/** Constants for GD service provider type.
+	 * This enumeration represents the type of service for which a service discovery
+	 * query is being issued. The <TT>serviceProviderType</TT> parameter of the
+	 *  \link GDiOS.GetServiceProvidersFor GetServiceProvidersFor:  (GDiOS)\endlink function always takes one of these values.
+	 */
 	public enum GDServiceProviderType{
 		GDServiceProviderApplication = 0,
+		/**< Application-based service. */
 		GDServiceProviderServer
+		/**< Server-based service. */
 	}
 
 	public enum GDUIColorTheme{
@@ -77,15 +146,113 @@ namespace GoodDynamics
 		DONE = 5
 	}
 
+
+	/**
+	 * \addtogroup gdfilesystemerrordomain GDFileSystem Error Domain
+	 * These constants can be used when handling errors returned by
+	 * \ref GDFileSystem, \ref GDCReadStream, and \ref GDCWriteStream functions.
+	 *
+	 * \{
+	 */
+	public enum GDFileSystemErr : uint {
+		GDFileSystemErrPathDoesntExist = 100,
+		GDFileSystemErrIOError = 101,
+		GDFileSystemErrPermissionError = 102,
+		GDFileSystemErrDirNotEmpty = 103,
+		GDFileSystemErrUnknownError = 500
+	}
+
+	/** \}
+	 */ 
+
+	/** \addtogroup iccconstants Good Inter-Container Communication Miscellaneous Constants
+	* These miscellaneous constants can be used with the Good Inter-Container
+	* Communication (ICC) sytem.
+	*
+	* For an overall description of ICC see the   \link GDService GDService class reference\endlink.
+	* 
+	* \{
+	*/
 	public enum GDTForegroundOption{
 		EPreferMeInForeground,
 		EPreferPeerInForeground,
 		ENoForegroundPreference
 	}
+	/** \}
+	 */
+
+	
+	/** Constants for GDSocket errors.
+	* This enumeration represents the type of a GDSocket error that is being
+	* notified. The <TT>error</TT> parameter of the
+	* \link GDSocketDelegate::onErr:inSocket: GDSocketDelegate::onErr:\endlink
+	* callback always takes one of these values.
+	*/
+	public enum GDSocketErrorType{
+		GDSocketErrorNone=0,
+		/**< No error.
+		 * This value is a placeholder for when the socket operation succeeded. The
+		 * error parameter never takes this value.
+		 */
+
+		GDSocketErrorNetworkUnvailable,
+		/**< Destination network not available.
+		 * This value indicates that the socket operation failed because the
+		 * destination network could not be reached.
+		 */
+
+		GDSocketErrorServiceTimeOut
+		/**< Socket operation timed out.
+		 * This value indicates that a socket operation timed out and did not
+		 * complete.
+		 */
+	}
+
+	/**
+	 * \addtogroup gdauthtokendomain Good Dynamics Authentication Token Error Domain
+	 * These constants can be used when handling Good Dynamics Authentication Token
+	 * request errors, in a \link GDAuthTokenDelegate GDAuthTokenDelegate\endlink implementation.
+	 *
+	 * \{
+	 */
 
 	public enum GDAuthTokenErr {
 		GDAuthTokenErrNotSupported = -2,
+		/**< The version of the Good Dynamics servers installed at the enterprise
+	     * does not support the Good Dynamics Authentication Token mechanism.
+	     */
 		GDAuthTokenErrRetry = -1
+		/**< An error occurred during token generation or communication.
+	     * Sending the same request later may not encounter the same condition, and
+	     * could succeed.
+	     */
+	}
+	/** \}
+ 	*/
+
+	/** Constants for HTTP Request ready states.
+	 * This enumeration represents the possible states of an HTTP request.
+	 *
+	 * \see http://www.w3.org/TR/XMLHttpRequest/#states
+	 *
+	 * Compare the value returned by \ref GDHttpRequest::getState "getState" to
+	 * these constants to check the ready state of the GDHttpRequest object.
+	 * (The XHR state names have been prefixed with <TT>GDHttpRequest_</TT> and
+	 * the standard values used.)
+	 */
+	public enum GDHttpRequestState{
+		GDHttpRequestUnsent = 0,
+		/**< Prior to the request being opened. */
+		GDHttpRequestOpened = 1,
+		/**< Ready to have headers added, and be sent. */
+		GDHttpRequestSent = 2,
+		/**< The request has been sent. */
+		GDHttpRequestHeadersReceived = 3,
+		/**< Sent, and response headers have been received. */
+		GDHttpRequestLoading = 4,
+		/**< Response headers and some data have been received. */
+		GDHttpRequestDone = 5
+		/**< All data has been received, or a permanent error has been encountered. */
 	}
 }
 
--- RemoteDb/RemoteDBSettings.cs
+++ RemoteDb/RemoteDBSettings.cs
@@ -11,10 +11,10 @@ namespace RemoteDb
 
 		const string SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS remoteSettings (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, serverHost  VARCHAR, serverPort  INTEGER, serverPriority  INTEGER, config      VARCHAR, copyPasteOn INTEGER, detailedLogOn INTEGER, userEmail  VARCHAR)";
 		
-		const string SQL_COUNT  = "SELECT COUNT(*) FROM remoteSettings";
+		const string SQL_COUNT = "SELECT COUNT(*) FROM remoteSettings";
 		const string SQL_INSERT = "INSERT INTO remoteSettings (serverHost, serverPort, serverPriority, config, copyPasteOn, detailedLogOn, userEmail) VALUES (?, ?, ?, ?, ?, ?, ?)";
 		const string SQL_UPDATE = "UPDATE remoteSettings SET serverHost = ?, serverPort = ?, serverPriority = ?, config = ?, copyPasteOn = ?, detailedLogOn = ?, userEmail = ?";
-		const string SQL_GET_SETTINGS   = "SELECT serverHost, serverPort, serverPriority, config, copyPasteOn, detailedLogOn, userEmail FROM remoteSettings";
+		const string SQL_GET_SETTINGS = "SELECT serverHost, serverPort, serverPriority, config, copyPasteOn, detailedLogOn, userEmail FROM remoteSettings";
 		const string DB_NAME = "remote_settings_db";
 
 		public RemoteDBSettings ()
@@ -22,15 +22,14 @@ namespace RemoteDb
 			InitializeDB ();
 		}
 
-		~RemoteDBSettings()
+		~RemoteDBSettings ()
 		{
 			SQLite3.Close (_database);
 		}
 
-		private SQLite3.Result GetResultFromIntPtr(IntPtr ptr)
+		private SQLite3.Result GetResultFromIntPtr (IntPtr ptr)
 		{
-			if (ptr == IntPtr.Zero) 
-			{
+			if (ptr == IntPtr.Zero) {
 				return SQLite3.Result.Error;
 			}
 
@@ -39,14 +38,13 @@ namespace RemoteDb
 			return (SQLite3.Result)ptrInt;
 		}
 
-		private bool InitializeDB()
+		private bool InitializeDB ()
 		{
 			int result = -1;
 			IntPtr db_namePtr = System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi (DB_NAME);
 			result = GoodDynamics.Sqlite3Enc.sqlite3enc_open (db_namePtr, out _database); 
 
-			if (result != 0) 
-			{
+			if (result != 0) {
 				return false;
 			}
 
@@ -56,14 +54,12 @@ namespace RemoteDb
 			IntPtr errorPtr = IntPtr.Zero;
 
 			var sqliteResult = SQLite3.Prepare2 (_database, queryPtr, query.Length, out statement, out errorPtr);
-			if (sqliteResult != SQLite3.Result.OK) 
-			{
+			if (sqliteResult != SQLite3.Result.OK) {
 				return false;
 			}
 
 			SQLite3.Result sqliteenum = (SQLite3.Result)SQLite3.Step (statement);
-			if ((SQLite3.Result)sqliteResult != SQLite3.Result.OK) 
-			{
+			if ((SQLite3.Result)sqliteResult != SQLite3.Result.OK) {
 				SQLite3.Finalize (statement);
 				return false;
 			}
@@ -72,20 +68,18 @@ namespace RemoteDb
 			return true;
 		}
 
-		public bool SettingsAlreadyStored()
+		public bool SettingsAlreadyStored ()
 		{
 			IntPtr statement = IntPtr.Zero;
 			int count = 0;
 
 			statement = SQLite3.Prepare2 (_database, SQL_COUNT);
-			if (statement == IntPtr.Zero) 
-			{
+			if (statement == IntPtr.Zero) {
 				return false;
 			}
 
 			var result = SQLite3.Step (statement);
-			if (result == SQLite3.Result.Row) 
-			{
+			if (result == SQLite3.Result.Row) {
 				count = SQLite3.ColumnInt (statement, 0);
 			}
 
@@ -94,52 +88,46 @@ namespace RemoteDb
 
 			return count > 0;
 		}
-			
-		public bool UpdateRemoteSettings(string serverHost, int port, int priority, string config, int copyPasteOn, int detailedLogOn, string userEmail)
+
+		public bool UpdateRemoteSettings (string serverHost, int port, int priority, string config, int copyPasteOn, int detailedLogOn, string userEmail)
 		{
 			IntPtr statement = IntPtr.Zero;
 			string sqlStatement = null;
 
-			if (SettingsAlreadyStored ()) 
-			{
+			if (SettingsAlreadyStored ()) {
 				sqlStatement = SQL_UPDATE;
-			} 
-			else 
-			{
+			} else {
 				sqlStatement = SQL_INSERT;
 			}
 
 			statement = SQLite3.Prepare2 (_database, sqlStatement);
-			if (statement == IntPtr.Zero) 
-			{
+			if (statement == IntPtr.Zero) {
 				return false;
 			}
 
-			SQLite3.BindText (statement, 1, System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi(serverHost), -1, IntPtr.Zero);
+			SQLite3.BindText (statement, 1, System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi (serverHost), -1, IntPtr.Zero);
 			SQLite3.BindInt (statement, 2, port);
 			SQLite3.BindInt (statement, 3, priority);
-			SQLite3.BindText (statement, 4, System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi(config), -1, IntPtr.Zero);
+			SQLite3.BindText (statement, 4, System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi (config), -1, IntPtr.Zero);
 			SQLite3.BindInt (statement, 5, copyPasteOn);
 			SQLite3.BindInt (statement, 6, detailedLogOn);
-			SQLite3.BindText (statement, 7, System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi(userEmail), -1, IntPtr.Zero);
+			SQLite3.BindText (statement, 7, System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi (userEmail), -1, IntPtr.Zero);
 
 			var result = SQLite3.Step (statement);
-			if (result != SQLite3.Result.Done) 
-			{
+			if (result != SQLite3.Result.Done) {
 				SQLite3.Finalize (statement);
 				return false;
 			}
 
 			result = SQLite3.Finalize (statement);
-			if (result != SQLite3.Result.OK) 
-			{
+			if (result != SQLite3.Result.OK) {
 				return false;
 			}
 
 			return true;
 		}
 
-		public NSMutableDictionary GetConfigSettings()
+		public NSMutableDictionary GetConfigSettings ()
 		{
 			NSMutableDictionary returnDictionary = new NSMutableDictionary ();
 
@@ -147,14 +135,12 @@ namespace RemoteDb
 			statement = SQLite3.Prepare2 (_database, SQL_GET_SETTINGS);
 			int detailedLogOn = -100;
 
-			if (statement == IntPtr.Zero) 
-			{
+			if (statement == IntPtr.Zero) {
 				return null;
 			}
 
 			var result = SQLite3.Step (statement);
-			if (result == SQLite3.Result.Row) 
-			{
+			if (result == SQLite3.Result.Row) {
 				IntPtr serverHostPtr = SQLite3.ColumnText (statement, 0);
 				string serverHost = System.Runtime.InteropServices.Marshal.PtrToStringAnsi (serverHostPtr);
 				int port = SQLite3.ColumnInt (statement, 1);
@@ -166,23 +152,19 @@ namespace RemoteDb
 				IntPtr userEmailPtr = SQLite3.ColumnText (statement, 6);
 				string userEmail = System.Runtime.InteropServices.Marshal.PtrToStringAnsi (userEmailPtr);
 
-				if (!String.IsNullOrWhiteSpace (serverHost)) 
-				{
-					GDAppServer firstServer = new GDAppServer (serverHost, new NSNumber(port), new NSNumber(priority));
+				if (!String.IsNullOrWhiteSpace (serverHost)) {
+					GDAppServer firstServer = new GDAppServer (serverHost, new NSNumber (port), new NSNumber (priority));
 					returnDictionary.Add (GDiOS.GDAppConfigKeyServers, firstServer);
 				}
-				if (!String.IsNullOrWhiteSpace (config)) 
-				{
-					returnDictionary.Add (GDiOS.GDAppConfigKeyConfig, new NSString(config));
+				if (!String.IsNullOrWhiteSpace (config)) {
+					returnDictionary.Add (GDiOS.GDAppConfigKeyConfig, new NSString (config));
 				}
-				returnDictionary.Add (GDiOS.GDAppConfigKeyCopyPasteOn, new NSNumber(copyPasteOn));
-				if (detailedLogOn != -100) 
-				{
-					returnDictionary.Add (GDiOS.GDAppConfigKeyDetailedLogsOn, new NSNumber(detailedLogOn));
+				returnDictionary.Add (GDiOS.GDAppConfigKeyCopyPasteOn, new NSNumber (copyPasteOn));
+				if (detailedLogOn != -100) {
+					returnDictionary.Add (GDiOS.GDAppConfigKeyDetailedLogsOn, new NSNumber (detailedLogOn));
 				}
-				if (!string.IsNullOrWhiteSpace(userEmail)) 
-				{
-					returnDictionary.Add (GDiOS.GDAppConfigKeyUserId, new NSString(userEmail));
+				if (!string.IsNullOrWhiteSpace (userEmail)) {
+					returnDictionary.Add (GDiOS.GDAppConfigKeyUserId, new NSString (userEmail));
 				}
 			}
 
